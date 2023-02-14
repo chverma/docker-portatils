c1ab19d12170e49193fd85c40cf259ca7fb3170d
chverma <538853+chverma@users.noreply.github.com>
2023-02-14 09:09:32 +0100

    Migrate to pm2

--- app/server.js
+++ app/server.js
@@ -20,9 +20,9 @@ const db = require("./app/models");
 
 db.sequelize.sync();
 // // drop the table if it already exists
-db.sequelize.sync({ force: true }).then(() => {
+/*db.sequelize.sync({ force: true }).then(() => {
    console.log("Drop and re-sync db.");
-});
+});*/
 
 
 app.use(express.static('./app/static'));
